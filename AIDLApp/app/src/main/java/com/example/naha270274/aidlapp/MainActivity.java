007f8db25cc03f57486cd6ef557be07d1ab13b02
narendra147h <narendra147h@gmail.com>
2018-10-03 13:49:20 +0530

    no message

---
+++ AIDLApp/app/src/main/java/com/example/naha270274/aidlapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.naha270274.aidlapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
